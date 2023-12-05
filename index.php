cf4ebef509e5ee6320cde350150982edde7420b7
DESKTOP-8KTM3PB\frede <frederic.vanovermeire@gmail.com>
2023-12-05 20:42:45 +0100

    reformat code

--- index.php
+++ index.php
@@ -2,27 +2,27 @@
 <main id="main-content">
     <?php include './includes/sidebar.php'; ?>
     <div id="content">
-    <section>
-        <div>
-            <h2>Login</h2>
-            <form action="./includes/login.inc.php" method="post">
-                <input type="text" name="username" placeholder="Username">
-                <input type="password" name="pwd" placeholder="Password">
-                <button type="submit">LOGIN</button>
-            </form>
-        </div>
-        <div>
-            <h2>Sign up</h2>
-            <form action="./includes/signup.inc.php" method="post">
-                <input type="text" name="username" placeholder="Username">
-                <input type="password" name="pwd" placeholder="Password">
-                <input type="password" name="pwdrepeat" placeholder="Repeat Password">
-                <button type="submit" name="submit">SIGN UP</button>
-            </form>
-        </div>
-    </section>
+        <section>
+            <div>
+                <h2>Login</h2>
+                <form action="./includes/login.inc.php" method="post">
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <button type="submit" name="submit">LOGIN</button>
+                </form>
+            </div>
+            <div>
+                <h2>Sign up</h2>
+                <form action="./includes/signup.inc.php" method="post">
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <input type="password" name="pwdrepeat" placeholder="Repeat Password">
+                    <button type="submit" name="submit">SIGN UP</button>
+                </form>
+            </div>
+        </section>
     </div>
-    </main>
+</main>
 </body>
 
 
