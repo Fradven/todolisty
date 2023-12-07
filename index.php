405ca1a6351c9359575bf8abe6b5d32ffc2c5de0
Alister Huysmans <alister.huysmans@gmail.com>
2023-12-07 01:46:44 +0100

    refactored and stylized login interface

--- index.php
+++ index.php
@@ -1,29 +1,68 @@
 <?php include './includes/header.php'; ?>
-<main id="main-content">
-    <?php include './includes/sidebar.php'; ?>
-    <div id="content">
-        <section>
-            <div>
-                <h2>Login</h2>
-                <form action="./includes/login.inc.php" method="post">
-                    <input type="text" name="username" placeholder="Username">
-                    <input type="password" name="pwd" placeholder="Password">
-                    <button type="submit" name="submit">LOGIN</button>
-                </form>
+    <main id="main-content">
+        <?php include './includes/sidebar.php'; ?>
+        <div id="content">
+            <div id="login-signup-card">
+                <div class="tabs">
+                    <button id="tab1" onclick="toggleTab('tab1','login-form')" class="tab tab-active">Login</button>
+                    <button id="tab2" onclick="toggleTab('tab2','signup-form')" class="tab">Signup</button>
+                </div>
+                <div class="login-signup-forms">
+                    <form id="login-form" class="tab-content tab-active" action="./includes/login.inc.php" method="post">
+                        <div class="form-container">
+                            <label for="l-uid">username</label>
+                            <input type="text" name="l-uid" required>
+                            <label for="l-pwd">password</label>
+                            <input type="password" name="l-pwd" required>
+                            <div class="form-actions">
+                                <button class="btn btn-outline" type="reset">Reset</button>
+                                <button class="btn" type="submit">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                    <form id="signup-form" class="tab-content" action="./includes/signup.inc.php" method="post">
+                        <div class="form-container">
+                            <label for="r-email">email</label>
+                            <input type="email" name="r-email"required>
+                            <label for="r-uid">username</label>
+                            <input type="text" name="r-uid" required>
+                            <label for="r-pwd">password</label>
+                            <input type="password" name="r-pwd" required>
+                            <label for="r-pwdrepeat">repeat password</label>
+                            <input type="password" name="r-pwdrepeat" required>
+                            <div class="form-actions">
+                                <button class="btn btn-outline" type="reset">Reset</button>
+                                <button class="btn" type="submit">Sign up</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="form-links">
+                    <a class="link" href="#">Help</a>
+                    <a class="link" href="#">Privacy</a>
+                    <a class="link" href="#">Terms</a>
+                </div>
             </div>
-            <div>
-                <h2>Sign up</h2>
-                <form action="./includes/signup.inc.php" method="post">
-                    <input type="text" name="username" placeholder="Username">
-                    <input type="password" name="pwd" placeholder="Password">
-                    <input type="password" name="pwdrepeat" placeholder="Repeat Password">
-                    <button type="submit" name="submit">SIGN UP</button>
-                </form>
-            </div>
-        </section>
-    </div>
-</main>
+        </div>    
+    </main>
+    <script>
+        function toggleTab(tabId, tabContentId) {
+            const tabs = document.querySelectorAll('.tab');
+            const tabContent = document.querySelectorAll('.tab-content');
+
+            tabContent.forEach(content => {
+            content.classList.remove('tab-active');
+            });
+            
+            tabs.forEach(tab => {
+            tab.classList.remove('tab-active');
+            });
+
+            const selectedTabContent = document.getElementById(tabContentId);
+            const selectedTab = document.getElementById(tabId);
+            selectedTabContent.classList.add('tab-active');
+            selectedTab.classList.add('tab-active');
+        }
+        </script>
 </body>
-
-
 </html>
