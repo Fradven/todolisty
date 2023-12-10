9f546964e166d3ec7bafb39f5ecd85226ee2f5d5
Alister Huysmans <alister.huysmans@gmail.com>
2023-12-10 21:11:10 +0100

    Improved Tableur responsive UI and harmonized css

--- tableur.php
+++ tableur.php
@@ -49,7 +49,7 @@ try {
                 <form action="" method="get">
                     <div class="filter-group">
                         <label for="sort">Trier par :</label>
-                        <select name="sort" id="sort">
+                        <select class="members-input" name="sort" id="sort">
                             <option value="title">Titre</option>
                             <option value="status">Statut</option>
                             <option value="priority_id">Priorité</option>
@@ -58,90 +58,87 @@ try {
                     </div>
                     <div class="filter-group">
                         <label for="direction">Ordre :</label>
-                        <select name="direction" id="direction">
+                        <select class="members-input" name="direction" id="direction">
                             <option value="ASC">Croissant</option>
                             <option value="DESC">Décroissant</option>
                         </select>
                     </div>
                     <div class="filter-group">
-                        <button type="submit" class="filter-button">Confirmer</button>
+                        <button type="submit" class="btn filter-button">Confirmer</button>
                     </div>
                 </form>
             </div>
-
-
-
-
-            <!-- Fin de la section de filtrage -->
-            <table class="tableau-tasks">
-                <thead>
-                    <tr>
-                        <th>Tâche</th>
-                        <th>Status</th>
-                        <th>Priorité</th>
-                        <th>Date de création</th>
-                        <th>Assignée à</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($tasks as $task): ?>
+            <div class="tableau-container">
+                <table class="tableau-tasks">
+                    <thead>
                         <tr>
-                            <td>
-                                <?= htmlspecialchars($task['title']) ?>
-                            </td>
-                            <td>
-                                <?= htmlspecialchars($task['status']) ?>
-                            </td>
-                            <td>
-                                <?= htmlspecialchars($task['priority']) ?>
-                            </td>
-                            <td>
-                                <?= date('d/m/Y H:i', strtotime($task['created_at'])) ?>
-                            </td>
-                            <td class="assigned-to">
-                                <?php
-                                // Déterminez le texte à afficher dans le cercle.
-                                $circleText = 'N/A'; // Texte par défaut si aucun utilisateur n'est assigné
-                                if (!empty($task['username'])) {
-                                    // Si un utilisateur est assigné, utilisez la première lettre en majuscule et la deuxième en minuscule.
-                                    $circleText = strtoupper(substr($task['username'], 0, 1));
-                                }
-
-                                // Définir un tableau associatif de correspondance entre caractères et couleurs aléatoires
-                                $colorMapping = [];
-
-                                // Générer des couleurs aléatoires pour chaque lettre de A à Z
-                                for ($i = 65; $i <= 90; $i++) {
-                                    $randomColor = '#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6); // Générer une couleur hexadécimale aléatoire
-                                    $character = chr($i); // Convertir le code ASCII en caractère
-                                    $colorMapping[$character] = $randomColor; // Associer la couleur au caractère
-                                }
-
-                                // Récupérer la couleur associée au caractère ou utiliser une couleur par défaut
-                                
-                                if (!function_exists('getUsernameColor')) {
-                                    // Define the getUsernameColor function
-                                    function getUsernameColor($username) {
-                                        // Use a hash function to generate a unique color based on the username
-                                        $hash = md5($username);
-                                        
-                                        // Take the first 6 characters of the hash to get a valid hex color code
-                                        $colorCode = '#' . substr($hash, 0, 6);
-                                
-                                        return $colorCode;
-                                    }
-                                }
-                                $backgroundColor = getUsernameColor($task['username']);
-                                ?>
-                                <div class="member-icon" style="background-color: <?= $backgroundColor ?>;">
-                                    <?= htmlspecialchars($circleText) ?>
-                                </div>
-                                <p><?= htmlspecialchars($task['username']) ?></p>
-                            </td>
+                            <th>Tâche</th>
+                            <th>Status</th>
+                            <th>Priorité</th>
+                            <th>Date de création</th>
+                            <th>Assignée à</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($tasks as $task): ?>
+                            <tr>
+                                <td>
+                                    <?= htmlspecialchars($task['title']) ?>
+                                </td>
+                                <td>
+                                    <?= htmlspecialchars($task['status']) ?>
+                                </td>
+                                <td>
+                                    <?= htmlspecialchars($task['priority']) ?>
+                                </td>
+                                <td>
+                                    <?= date('d/m/Y H:i', strtotime($task['created_at'])) ?>
+                                </td>
+                                <td class="assigned-to">
+                                    <?php
+                                    // Déterminez le texte à afficher dans le cercle.
+                                    $circleText = 'N/A'; // Texte par défaut si aucun utilisateur n'est assigné
+                                    if (!empty($task['username'])) {
+                                        // Si un utilisateur est assigné, utilisez la première lettre en majuscule et la deuxième en minuscule.
+                                        $circleText = strtoupper(substr($task['username'], 0, 1));
+                                    }
+    
+                                    // Définir un tableau associatif de correspondance entre caractères et couleurs aléatoires
+                                    $colorMapping = [];
+    
+                                    // Générer des couleurs aléatoires pour chaque lettre de A à Z
+                                    for ($i = 65; $i <= 90; $i++) {
+                                        $randomColor = '#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6); // Générer une couleur hexadécimale aléatoire
+                                        $character = chr($i); // Convertir le code ASCII en caractère
+                                        $colorMapping[$character] = $randomColor; // Associer la couleur au caractère
+                                    }
+    
+                                    // Récupérer la couleur associée au caractère ou utiliser une couleur par défaut
+                                    
+                                    if (!function_exists('getUsernameColor')) {
+                                        // Define the getUsernameColor function
+                                        function getUsernameColor($username) {
+                                            // Use a hash function to generate a unique color based on the username
+                                            $hash = md5($username);
+                                            
+                                            // Take the first 6 characters of the hash to get a valid hex color code
+                                            $colorCode = '#' . substr($hash, 0, 6);
+                                    
+                                            return $colorCode;
+                                        }
+                                    }
+                                    $backgroundColor = getUsernameColor($task['username']);
+                                    ?>
+                                    <div class="member-icon" style="background-color: <?= $backgroundColor ?>;">
+                                        <?= htmlspecialchars($circleText) ?>
+                                    </div>
+                                    <p><?= htmlspecialchars($task['username']) ?></p>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </section>
     </div>
 </main>
